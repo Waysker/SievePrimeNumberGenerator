082cab342d48bfe821bde7f650e1d33ce022d1cb
Łukasz Ściga <scigalukasz@gmail.com>
2018-01-04 20:01:36 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,8 @@ public class Main {
         Sieve sieve = new Sieve();
         Generator generator = new Generator();
 
+        // TODO name variables with meaningful names, tell viewer what this stuff IS, what JOB DOES IT HAVE, don't state
+        // obvious like it's argument zero. Tell me what this is FOR, with its name, so I don't need to analyze code to know it
         int arg0 = Integer.parseInt(args[0]);
         System.out.println(generator.Integers(arg0).get(4));
         List<Integer> list = sieve.zeroRemoval(sieve.sieve(generator.Integers(arg0)));
